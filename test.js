3497e9d01cbe3a381b553da094a66cf8d4d81072
LevDiakov <381208dl@gmail.com>
2023-02-12 16:28:16 -0300

    Nhtnbq rjvvbn b b dtnre cjplf`v

--- test.js
+++ test.js
@@ -3,20 +3,21 @@ let materialPrice = {
   'stone': 505,
   'brick': 5555 // внёс изменения в значение
 };
+Вношу серьёзные изменения в код для второго коммита
+Проверяю команду git diff
 
 let house = {
   calculateSquare: function () {
   return this.rooms * this.coefficient * this.rooms;
 },
-  rooms: 100000,
-  floors: 500000,
-  material: 'red',
-  coefficient: 10.5,
-  calculatePrice: function () {
+rooms: 100000,
+floors: 500000,
+material: 'red',
+coefficient: 10.5,
+calculatePrice: function () {
   return this.calculateSquare() * materialPrice[this.material];
 }
 };
 
 console.log(house.calculatePrice())
 
-Вношу серьёзные изменения в код для второго коммита
